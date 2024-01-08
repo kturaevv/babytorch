e4383bb477c936b47dbdd9498167db3df8e5848f
kturaevv <kturaevv@gmail.com>
2024-01-08 22:30:42 +0500

    configure clangd linter

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@ int main() {
     fmt::print("Auto-diff project!\n");
 
     using namespace scalar;
-
     using namespace boost::lambda;
+
     typedef std::istream_iterator<int> in;
 
     std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " ");
