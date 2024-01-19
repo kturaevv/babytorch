6059011dea756e8c77fe65d3ce7f2f0fb5e89c54
kturaevv <kturaevv@gmail.com>
2024-01-19 19:56:38 +0500

    edit rand allocate memory in heap

--- src/babytorch/utils.hpp
+++ src/babytorch/utils.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <vector>
 
 namespace utils {
-    std::vector<double> rand(size_t size);
-    std::vector<double> rand(size_t size, int min, int max);
+    std::unique_ptr<std::vector<double>> rand(size_t size);
+    std::unique_ptr<std::vector<double>> rand(size_t size, int min, int max);
 
     template <typename T>
     void print_vec(std::vector<T>& vec) {
