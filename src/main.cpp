b6e01ad504aadef06ecd9a3f31bbdd9cb5ef2af5
kturaevv <kturaevv@gmail.com>
2024-02-07 20:42:21 +0500

    edit example usage

--- src/main.cpp
+++ src/main.cpp
@@ -8,47 +8,30 @@
 
 #include "./babytorch/scalar.hpp"
 #include "./babytorch/tensor.hpp"
+#include "./babytorch/tensor_data.hpp"
 
 int main() {
     fmt::print("Autograd project!\n\n");
 
     using scalar::Scalar;
-    auto x = Scalar::create(1.0);
-    auto y = Scalar::create(2.0);
-    auto z = Scalar::create(3.0);
-    auto k = Scalar::create(4.0);
-    auto j = Scalar::create(5.0);
-
-    fmt::print("Example -> Scalars:\n\n");
-    fmt::print("x = {}", *x);
-    fmt::print("y = {}", *y);
-    fmt::print("z = {}", *z);
-    fmt::print("k = {}", *k);
-    fmt::print("j = {}", *j);
-    fmt::print("\n");
-
+    auto x      = Scalar::create(1.0);
+    auto y      = Scalar::create(2.0);
+    auto z      = Scalar::create(3.0);
+    auto k      = Scalar::create(4.0);
+    auto j      = Scalar::create(5.0);
     auto result = (x * y + z - k / j);
-    fmt::print("result = (x * y + z - k / j) => {}\n", *result);
-
     result->backward();
-    fmt::print("After .backward() pass, Scalars are upgraded: \n");
-    fmt::print("x = {}", *x);
-    fmt::print("y = {}", *y);
-    fmt::print("z = {}", *z);
-    fmt::print("k = {}", *k);
-    fmt::print("\n\n");
+    fmt::print("{}\n", *result);
 
     using tensor::Tensor;
+    auto tensor_sample = Tensor(3, 3, 5);
+    auto a             = Tensor(3, 3, 5);
+    auto b             = Tensor(3, 1, 5);
+    auto c             = Tensor(5);
+    auto d             = Tensor(3, 3, 1);
+    auto e             = Tensor(3, 5);
+    auto tensor_result = a / 1.2 + b * c / d - 3 - e;
+    fmt::print("{}", tensor_result);
 
-    fmt::print("Example -> Tensors:\n\n");
-
-    auto a = Tensor(3, 3, 5);
-    fmt::print("Creating an n-dimensional tensor of shape (3,3,5) ");
-    fmt::print("i.e. Tensor(3,3,5) ->:\n{} \n", a);
-    fmt::print("Indexing to a certain dimensions would return a sub-Tensor:\n");
-
-    fmt::print("Tensor[1] = {} \n", a[1]);
-    fmt::print("Tensor[1, 2] = {} \n", a[1, 2]);
-    fmt::print("Tensor[1, 2, 3] = {} \n", a[1, 2, 3]);
     return 0;
 }
